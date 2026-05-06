b8f5ca6c3665e7a40991102029754342422443b9
choked-zzZ <azureskyemusic@gmail.com>
2026-05-06 17:19:57 +0800

    [Feat(Opt)]: Support function purity detection.

---
+++ raana_ir/src/opt/analysis_passes/pure_function.rs
@@ -0,0 +1,56 @@
+use crate::opt::prelude::*;
+
+pub fn is_pure_function(program: &Program, func: Function) -> bool {
+    // All library function have side-effect.
+    // And function should have unique name.
+    let data = program.func_data(func);
+    if matches!(
+        data.name(),
+        "getint"
+            | "getch"
+            | "getfloat"
+            | "getarray"
+            | "getfarray"
+            | "putint"
+            | "putch"
+            | "putfloat"
+            | "putarray"
+            | "putfarray"
+            | "putf"
+    ) {
+        return false;
+    };
+    for bb in data.layout().basicblocks() {
+        for &inst in bb.insts() {
+            match data.inst_data(inst).kind() {
+                InstKind::Load(load) => {
+                    if load.src().is_global() {
+                        return false;
+                    }
+                }
+                InstKind::Store(store) => {
+                    if store.src().is_global() || store.dest().is_global() {
+                        return false;
+                    }
+                }
+                InstKind::Call(call) => {
+                    if !is_pure_function(program, call.callee()) {
+                        return false;
+                    }
+                }
+                InstKind::GetElemPtr(gep) => {
+                    if gep.base().is_global() {
+                        return false;
+                    }
+                }
+                InstKind::GetPtr(gp) => {
+                    if gp.base().is_global() {
+                        return false;
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+    true
+}

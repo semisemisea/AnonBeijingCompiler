2573fd2fd98d05598a0d098d5d6ef13c4359d2b1
choked-zzZ <azureskyemusic@gmail.com>
2026-05-06 14:25:23 +0800

    [Feat(IR)]: Migrate `sr` pass.

--- raana_ir/src/opt/passes/sr.rs
+++ raana_ir/src/opt/passes/sr.rs
@@ -1,77 +1,62 @@
 use crate::{
-    ir::{BinaryOp, Function, FunctionData, InstKind, builder_trait::LocalInstBuilder},
-    opt::pass::Pass,
+    ir::{BinaryOp, InstKind, arena::Arena, builder_trait::*},
+    opt::pass::{ArenaContext, Pass},
 };
 
 pub struct StrengthReduction;
 
 impl Pass for StrengthReduction {
-    fn run_on(&mut self, func: Function, data: &mut FunctionData) {
+    fn run_on(&self, data: &mut ArenaContext<'_>) {
         self.local_reduction(data);
     }
 }
 
 impl StrengthReduction {
-    fn local_reduction(&self, data: &mut FunctionData) {
+    fn local_reduction(&self, data: &mut ArenaContext<'_>) {
         let to_change = data
             .layout()
-            .bbs()
+            .basicblocks()
             .iter()
-            .flat_map(|(_, node)| node.insts().keys())
-            .filter(|&&val| matches!(data.dfg().value(val).kind(), InstKind::Binary(_)))
+            .flat_map(|layout| layout.insts())
+            .filter(|&&val| matches!(data.inst_data(val).kind(), InstKind::Binary(_)))
             .copied()
             .collect::<Vec<_>>();
 
         for val in to_change {
-            let InstKind::Binary(binary) = data.dfg().value(val).kind() else {
+            let InstKind::Binary(binary) = data.inst_data(val).kind() else {
                 unreachable!()
             };
             match binary.op() {
                 BinaryOp::Mul => {
-                    if let InstKind::Integer(int) = data.dfg().value(binary.lhs()).kind() {
+                    if let InstKind::Integer(int) = data.inst_data(binary.lhs()).kind() {
                         if int.value().is_positive() && (int.value() as u32).is_power_of_two() {
                             let po2 = int.value() as u32;
                             let shl_base = binary.lhs();
-                            let shl_offset = data
-                                .dfg_mut()
-                                .new_value()
-                                .integer(po2.trailing_zeros() as i32);
-                            data.dfg_mut().replace_value_with(val).binary(
-                                BinaryOp::Shl,
-                                shl_base,
-                                shl_offset,
-                            );
+                            let shl_offset =
+                                data.new_local_inst().integer(po2.trailing_zeros() as i32);
+                            data.replace_inst_with(val)
+                                .binary(BinaryOp::Shl, shl_base, shl_offset);
                         }
-                    } else if let InstKind::Integer(int) = data.dfg().value(binary.rhs()).kind() {
+                    } else if let InstKind::Integer(int) = data.inst_data(binary.rhs()).kind() {
                         if int.value().is_positive() && (int.value() as u32).is_power_of_two() {
                             let po2 = int.value() as u32;
                             let shl_base = binary.lhs();
-                            let shl_offset = data
-                                .dfg_mut()
-                                .new_value()
-                                .integer(po2.trailing_zeros() as i32);
-                            data.dfg_mut().replace_value_with(val).binary(
-                                BinaryOp::Shl,
-                                shl_base,
-                                shl_offset,
-                            );
+                            let shl_offset =
+                                data.new_local_inst().integer(po2.trailing_zeros() as i32);
+                            data.replace_inst_with(val)
+                                .binary(BinaryOp::Shl, shl_base, shl_offset);
                         }
                     }
                 }
                 BinaryOp::Div => {
-                    if let InstKind::Integer(int) = data.dfg().value(binary.rhs()).kind() {
+                    if let InstKind::Integer(int) = data.inst_data(binary.rhs()).kind() {
                         if int.value().is_positive() && (int.value() as u32).is_power_of_two() {
                             let po2 = int.value() as u32;
                             let shl_base = binary.lhs();
-                            let shl_offset = data
-                                .dfg_mut()
-                                .new_value()
-                                .integer(po2.trailing_zeros() as i32);
-                            data.dfg_mut().replace_value_with(val).binary(
-                                BinaryOp::Sar,
-                                shl_base,
-                                shl_offset,
-                            );
+                            let shl_offset =
+                                data.new_local_inst().integer(po2.trailing_zeros() as i32);
+                            data.replace_inst_with(val)
+                                .binary(BinaryOp::Sar, shl_base, shl_offset);
                         }
                     }
                 }

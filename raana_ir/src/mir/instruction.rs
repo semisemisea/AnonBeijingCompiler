3ea607559cbe3280ee79652b4dd728556fcd5976
choked-zzZ <azureskyemusic@gmail.com>
2026-05-11 15:36:12 +0800

    [Feat(MIR)]: MIR init commit. - Create basic structure/description of MIR. - Add helper function in `Type`.

---
+++ raana_ir/src/mir/instruction.rs
@@ -0,0 +1,17 @@
+use crate::mir::operand::Inst;
+
+pub struct BasicBlock {
+    pub name: String,
+    pub insts: Vec<Inst>,
+}
+
+pub struct Function {
+    pub name: String,
+    pub blocks: Vec<BasicBlock>,
+    pub stack_size: u32,
+}
+
+pub struct Program {
+    pub global: Vec<Inst>,
+    pub funcs: Vec<Function>,
+}
